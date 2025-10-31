1e53a9f97a597c25ca8b668e55b2a2f4e6433123
ali9727asim-code <ali.9727asim@gmail.com>
2025-10-31 11:08:51 +0500

    Stage 3 completed

--- task/game.py
+++ task/game.py
@@ -1,7 +1,19 @@
+names=["John","Jack"]
+def display(pencils):
+    line=""
+    for i in range(pencils):
+        line+="|"
+    print(line)
 pencils=int(input("How many pencils would you like to use:"))
 name=input("Who will be the first (John, Jack):")
-line=""
-for i in range(pencils):
-    line+="|"
-print(line)
-print(f"{name} is going first!")
+display(pencils)
+indx = names.index(name)
+while True:
+    print(f"{names[indx]}'s turn:")
+    n=int(input())
+    pencils-=n
+    if pencils<=0:
+        break
+    display(pencils)
+    indx=1-indx
+
