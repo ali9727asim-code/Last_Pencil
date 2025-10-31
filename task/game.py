3058d2fa75d5a7ed4b3c648af365e48e0624813b
ali9727asim-code <ali.9727asim@gmail.com>
2025-10-31 06:56:13 +0500

    Enter name of user and number of pencils.

--- task/game.py
+++ task/game.py
@@ -1,2 +1,7 @@
-print("||||||||")
-print("Your turn!")
+pencils=int(input("How many pencils would you like to use:"))
+name=input("Who will be the first (John, Jack):")
+line=""
+for i in range(pencils):
+    line+="|"
+print(line)
+print(f"{name} is going first!")
